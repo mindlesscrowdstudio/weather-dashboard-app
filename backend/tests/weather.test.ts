726723066aa0e73d01ae4cf89685e868f94b85b1
Melina Cisneros <melinacisneross@gmail.com>
2025-07-22 20:14:20 -0600

    test(weather): write FAILING test for current temp retrieval
     Failing test for fetching the current temperature. expected to fail until the feature is implemented.

---
+++ backend/tests/weather.test.ts
@@ -0,0 +1,14 @@
+import request from 'supertest';
+//import app from '../src/app';
+
+
+//Cycle 1
+describe('GET /api/weather/current/:city', () => {
+  it('should return 404 if the city is NOT found', async () => {
+    // We'll need to mock the fetch API later to simulate this
+    const response = await request(app)
+      .get('/api/weather/current/unexistencedcity');
+    expect(response.status).toBe(404);
+    expect(response.body).toHaveProperty('message', 'Sorry, City not found');
+  });
+});
