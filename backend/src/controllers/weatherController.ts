5e9f9d6752a88da91978ce192411bd4b42830ed6
Melina Cisneros <melinacisneross@gmail.com>
2025-07-22 23:01:37 -0600

    test(weather): GREEN Scenario for Forecast API
    Add Passed scenario for a forecast api request.
    Add 404 scenario
    Add 500 scenario
    Implement a router for the forecast/:city

--- backend/src/controllers/weatherController.ts
+++ backend/src/controllers/weatherController.ts
@@ -27,6 +27,37 @@ export class WeatherController {
       });
     }
   }
+
+  // Cycle 2: Implementing the forecast endpoint
+  async getForecast(req: Request, res: Response) {
+    try {
+      const { city } = req.params;
+      const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${OPENWEATHER_API_KEY}&units=metric`;
+
+      const apiResponse = await fetch(url);
+
+      if(!apiResponse.ok) {
+        // change messafge to differ from current weather
+        return res.status(404).json({ message: 'city not found' });
+      }
+      const forecastData = await apiResponse.json();
+
+      console.log('--- Forecast data received: ---');
+      // Log with full object to see full data
+      //console.log(JSON.stringify(forecastData, null, 2));
+      // Node mode
+      console.dir(forecastData, { depth: null });
+
+      // happy scenario:
+      res.status(200).json(forecastData);
+
+    } catch (error) {
+      res.status(500).json({
+        message: 'Internal server error',
+      });
+    }
+  }
 }
+
 
 export const weatherController = new WeatherController();
--- backend/src/routes/weather.ts
+++ backend/src/routes/weather.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 import { weatherController } from '../controllers/weatherController';
 
 const router = Router();
+
 router.get('/current/:city', weatherController.getCurrentWeather);
+router.get('/forecast/:city', weatherController.getForecast);
 
 export default router;
