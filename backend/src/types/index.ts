ef35dd635af769fd695d3e553abf6c0ab6be1573
Melina Cisneros <melinacisneross@gmail.com>
2025-07-24 01:14:57 -0600

    Add SearchHistoryItem interface to types
    Introduces the SearchHistoryItem interface. This prepares the codebase for features related to user search history.

--- backend/src/types/index.ts
+++ backend/src/types/index.ts
@@ -5,3 +5,12 @@ export interface FavoriteCity {
   country_code: string;
   added_at: Date;
 }
+
+export interface SearchHistoryItem {
+  id: number;
+  user_id?: number;
+  city_name: number;
+  country_code: string;
+  searched_at: Date;
+  weather_data: string; // change to WeatherData interface
+}
