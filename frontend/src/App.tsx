e5b29f6f678d1ade49440adf8ade78adae789cfd
Melina Cisneros <melinacisneross@gmail.com>
2025-07-28 10:56:41 -0600

    Update App file for content and styles
    Remove  content and adjust classes to make wider the application container

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,14 @@
 
-/* import { DashboardPage } from '@/pages/DashboardPage'; */
+import WeatherDashboard from '@/pages/DashboardPage';
 
 function App() {
 
 
   return (
     <>
-      <div className="min-h-screen bg-gray-100 flex flex-col items-center p-4">
-        test
-       {/* <DashboardPage /> */}
-    </div>
+      <div className="min-h-screen bg-gray-50 p-6">
+       <WeatherDashboard />
+      </div>
     </>
   )
 }
